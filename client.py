cb6cb4993503de876be807e9e09e17f404638e47
Danni (Rachel) Shang <racheldshang@gmail.com>
2024-10-01 07:30:46 -0400

    client-server is now asynchronous and can support multiple client connections. client/server can send/receive files to each other, and files are stored in a dictionary in the server's memory space

--- client.py
+++ client.py
@@ -3,36 +3,45 @@ import os
 import threading
 
 def send_file(file_name, client_socket):
-    # Send 'post' message type
     client_socket.send(b'post')
-    
-    # Send file name
-    client_socket.send(file_name.encode())
-
-    # Send the file content
+    file_name_bytes = file_name.encode('utf-8')
+    file_name_size = len(file_name_bytes)
+    client_socket.send(file_name_size.to_bytes(4, 'big')) 
+    client_socket.send(file_name_bytes)
+    print("Client is sending file to server: ", file_name)
+    file_size = os.path.getsize(file_name)
+    client_socket.send(file_size.to_bytes(8, 'big'))
     with open(file_name, 'rb') as f:
         while True:
             file_data = f.read(1024)
             if not file_data:
                 break
             client_socket.send(file_data)
+    f.close()
+    print("File sent.")
 
 def get_file(file_name, client_socket):
-    # Send 'get' message type
     client_socket.send(b'get')
-    
-    # Send file name to retrieve
-    client_socket.send(file_name.encode())
-    
-    # Receive the file from the server
+
+    file_name_bytes = file_name.encode('utf-8')
+    file_name_size = len(file_name_bytes)
+    client_socket.send(file_name_size.to_bytes(4, 'big'))  
+    client_socket.send(file_name_bytes)
+    print("Client is getting file from server: ", file_name)
+    file_size = int.from_bytes(client_socket.recv(8), 'big')
+
+    bytes_received = 0
     with open(f'downloaded_{file_name}', 'wb') as f:
-        while True:
+        while bytes_received < file_size:
             file_data = client_socket.recv(1024)
             if not file_data:
                 break
             f.write(file_data)
+            bytes_received += len(file_data)
+    f.close()
+    print("File retrieved.")
 
-def start_client(message):
+def start_client(file_name):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     server_address = ('localhost', 65432)  
@@ -40,33 +49,27 @@ def start_client(message):
     client_socket.connect(server_address)
     
     try:
-        # Example file sending (post)
-        # file_name = "test.txt"
-        print(f"Sending: {message}")
-        client_socket.sendall(message.encode('utf-8'))
-        
-        data = client_socket.recv(1024)
-        print(f"Received from server: {data.decode('utf-8')}")
-        # send_file(file_name, client_socket)
-        # print("Send file to server")
-        
-        # # Example file receiving (get)
-        # get_file(file_name, client_socket)
-        # print("Received file from server")
-    
+        send_file(file_name, client_socket)
+        response = client_socket.recv(1024)
+        print(f"Server response: {response}")
+        get_file(file_name, client_socket)
+
     finally:
         print("Closing connection")
         client_socket.close()
 
-def test_connect_two_clients():
-    thread1 = threading.Thread(target=start_client, args=("Hello from client 1!",))
-    thread2 = threading.Thread(target=start_client, args=("Hello from client 2!",))
+def test_multiple_clients():
+    thread1 = threading.Thread(target=start_client, args=("username.csv",))
+    thread2 = threading.Thread(target=start_client, args=("username-password-recovery-code.csv",))
+    thread3 = threading.Thread(target=start_client, args=("username-password-recovery-code.csv",))
 
     thread1.start()
     thread2.start()
+    thread3.start()
 
     thread1.join()
-    thread2.join()
+    thread2.join() 
+    thread3.join()
 
 if __name__ == "__main__":
-    test_connect_two_clients()
+    test_multiple_clients()
--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import socket
 import threading
-from http.server import SimpleHTTPRequestHandler, HTTPServer
 import os
 
-latest_message = "No messages received yet."
-data_store = {}
+# what to do if a file of the same name has already been saved by the server?
+
+file_store = {}
 
 def start_tcp_server():
     global latest_message
@@ -12,8 +12,8 @@ def start_tcp_server():
     server_address = ('0.0.0.0', 65432)
     print(f"Starting TCP server on {server_address[0]}:{server_address[1]}")
     server_socket.bind(server_address)
-    server_socket.listen(1)
-
+    server_socket.listen(5)
+ 
     while True:
         client_socket, addr = server_socket.accept()
         print(f"Accepted connection from {addr}")
@@ -24,74 +24,57 @@ def handle_client(client_socket):
     try:
         print(f"Connection established with client")
         while True:
-            data = client_socket.recv(1024)
-            if data:
-                latest_message = data.decode('utf-8')
-                print(f"Received: {latest_message}")
-                response = "Server received: " + latest_message
-                client_socket.sendall(response.encode('utf-8'))
+            method = client_socket.recv(4).decode('utf-8')
+            if method == 'post':
+                file_name_size = int.from_bytes(client_socket.recv(4), 'big')
+                if file_name_size > 0:
+                    file_name = client_socket.recv(file_name_size).decode('utf-8')
+                    print("Server is receiving file from client: ", file_name)
+                    if not os.path.exists("server_files"):
+                        os.makedirs("server_files")
+                    file_size = int.from_bytes(client_socket.recv(8), 'big')
+                    file_path = f"server_files/{file_name}"
+                    bytes_received = 0
+                    with open(file_path, 'wb') as f:
+                        while bytes_received < file_size:
+                            file_data = client_socket.recv(1024)
+                            if not file_data:
+                                break
+                            f.write(file_data)
+                            bytes_received += len(file_data)
+                    file_store[file_name] = file_path
+                    f.close()
+                    print("File retrieved.")
+                    client_socket.send(b'File received and stored')
+                else:
+                    break
+            elif method == 'get':
+                file_name_size = int.from_bytes(client_socket.recv(4), 'big')
+                if file_name_size > 0:
+                    file_name = client_socket.recv(file_name_size).decode('utf-8')
+                    if file_name in file_store:
+                        file_path = file_store[file_name]
+                        print("Server is retrieving file for client: ",file_name)
+                        file_size = os.path.getsize(file_path)
+                        client_socket.send(file_size.to_bytes(8, 'big'))
+ 
+                        with open(file_path, 'rb') as f:
+                            while True:
+                                file_data = f.read(1024)
+                                if not file_data:
+                                    break
+                                client_socket.send(file_data)
+                        f.close()
+                        print("File sent.")
+                    else:
+                        print("File not found: ", file_name)
+                else:
+                    break
             else:
                 break
-            # msg_type = client_socket.recv(1024).decode()
-            # if msg_type == b'post':
-            #     # Receive file metadata
-            #     file_name = client_socket.recv(1024).decode()
-            #     file_path = f"server_files/{file_name}"
-                
-            #     # Receive the file from the client
-            #     with open(file_path, 'wb') as f:
-            #         while True:
-            #             file_data = client_socket.recv(1024)
-            #             if not file_data:
-            #                 break
-            #             f.write(file_data)
 
-            #     # Store file metadata in dictionary
-            #     file_store[file_name] = file_path
-            #     client_socket.send(b'File received and stored')
-
-            # elif msg_type == b'get':
-            #     # Receive file name client wants
-            #     file_name = client_socket.recv(1024).decode()
-
-            #     # Check if file exists in the store
-            #     if file_name in file_store:
-            #         file_path = file_store[file_name]
-
-            #         # Send the file to the client
-            #         with open(file_path, 'rb') as f:
-            #             while True:
-            #                 file_data = f.read(1024)
-            #                 if not file_data:
-            #                     break
-            #                 client_socket.send(file_data)
-
-            #         client_socket.send(b'File sent')
-            #     else:
-            #         client_socket.send(b'File not found')
-
-            # else:
-            #     client_socket.send(b'Invalid command')
     finally:
         client_socket.close()
 
-class MyHandler(SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(f"<html><body><h1>Latest Message: {latest_message}</h1></body></html>".encode('utf-8'))
-
-def start_http_server():
-    httpd = HTTPServer(('0.0.0.0', 8080), MyHandler)
-    print("Starting HTTP server on port 8080...")
-    httpd.serve_forever()
-
 if __name__ == "__main__":
-    if not os.path.exists("server_files"):
-        os.mkdir("server_files")
-    tcp_thread = threading.Thread(target=start_tcp_server)
-    tcp_thread.daemon = True
-    tcp_thread.start()
-
-    start_http_server()
+    start_tcp_server()
